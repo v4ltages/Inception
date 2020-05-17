4490f260c24fb62ef9f9fd0cae73caceea2bc96c
v4ltages <2614607-v4ltages@users.noreply.gitlab.com>
2020-05-17 22:19:41 +0300

    ffmpeg improvements, not working fully currently.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const axios = require('axios')
 const https = require('https');
 const fs = require('fs');
+const ffprobeclient = require('ffprobe-client')
 
 const readline = require('readline').createInterface({
     input: process.stdin,
@@ -11,14 +12,30 @@ const videofile = fs.createWriteStream(`./temp/videofile.mp4`);
 const audiofile = fs.createWriteStream(`./temp/audiofile.mp3`);
 readline.question(`Insert a Coub ID: `, (CoubID) => {
     axios.get(`https://coub.com/api/v2/coubs/`+CoubID).then(response => {
+        var CoubName = (response.data.title)
+        // CoubName = CoubName.replace(/\s+/g, ''); // Remove's all spacing in the string of CoubName
+        // CoubName = CoubName.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, ''); // Remove's all special characters in the string of CoubName
         https.get(response.data.file_versions.html5.video.higher.url, function(videoresponse) {
-            console.log(response)
             videoresponse.on(`end`, () => {
                 https.get(response.data.file_versions.html5.audio.high.url, function(audioresponse) {
                     audioresponse.pipe(audiofile);
                     audioresponse.on(`end`, () => {
+                        if (fs.existsSync(`./output/${CoubName}.mp4`)) {
+                            fs.unlink(`./output/${CoubName}.mp4`, (err) => {
+                                if (err) throw err;
+                                console.log(`Overwriting ./output/${CoubName}.mp4`);
+                            });
+                        }
                         const { exec } = require("child_process");
-                        exec("ffmpeg -i ./temp/videofile.mp4 -i ./temp/audiofile.mp3 ./temp/out.mp4", (error, stdout, stderr) => {
+                        ffprobeclient('./temp/videofile.mp4')
+                            .then(vfd => console.log(`Video file duration: `+vfd.format.duration))
+                            .catch(err => console.error(err));
+                        ffprobeclient('./temp/audiofile.mp3')
+                            .then(afd => console.log(`Audio file duration: `+afd.format.duration))
+                            .catch(err => console.error(err));
+                        var ffmpegloops = Math.floor(afd.format.duration/vfd.format.duration)
+                        ffmpegloops = ffmpegloops.replace()
+                        exec(`ffmpeg -i ./temp/videofile.mp4 -i ./temp/audiofile.mp3 "./output/${CoubName}.mp4"`, (error, stdout, stderr) => {
                             if (error) {
                                 console.log(`error: ${error.message}`);
                                 return;
